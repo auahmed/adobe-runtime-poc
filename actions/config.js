6da7ed48a0446561e1f44fcb8b123062c2e37ee4
Attiq Ahmed <attiqahmed92@gmail.com>
2022-09-07 15:31:18 -0400

    update config

--- actions/config.js
+++ actions/config.js
@@ -2,10 +2,10 @@ module.exports = {
   audienceId: "1821808",
   tenant: "aknbusinessolamerptrsd",
   credentials: {
-      clientId : "765b6965229a4f83a2aa70883eaefce7", //Application ID (Client ID)
-      technicalAccountId : "EA81194560BF71260A495E5D@techacct.adobe.com", //Technical account ID
-      orgId : "ECE34E785E54ED240A495C13@AdobeOrg", // Org ID
-      clientSecret : "p8e-fQvm97WMnFwNiSgJI0jRUryUXj4GZpya", //Client Secret
+      clientId : "", //Application ID (Client ID)
+      technicalAccountId : "@techacct.adobe.com", //Technical account ID
+      orgId : "@AdobeOrg", // Org ID
+      clientSecret : "", //Client Secret
       metaScopes : "ent_marketing_sdk", // Metascopes
       privateKey : ""
   }
